574e2723b9cb612747270a2135604bcd5e13be41
chamudithalakshan <chamudithawickramarathna@gmail.com>
2023-04-05 17:48:16 +0530

    RoomDaoAdded

---
+++ src/lk/ijse/hibernate/dao/custom/RoomDao.java
@@ -0,0 +1,8 @@
+package lk.ijse.hibernate.dao.custom;
+
+import lk.ijse.hibernate.dao.CrudDAO;
+import lk.ijse.hibernate.entity.Rooms;
+
+public interface RoomDao extends CrudDAO<Rooms,String> {
+
+}
---
+++ src/lk/ijse/hibernate/dao/custom/impl/RoomDaoImpl.java
@@ -0,0 +1,33 @@
+package lk.ijse.hibernate.dao.custom.impl;
+
+import lk.ijse.hibernate.dao.custom.RoomDao;
+import lk.ijse.hibernate.entity.Rooms;
+
+import java.util.List;
+
+public class RoomDaoImpl implements RoomDao {
+    @Override
+    public boolean add(Rooms rooms) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean update(Rooms rooms) throws Exception {
+        return false;
+    }
+
+    @Override
+    public Rooms search(String s) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Rooms> getAll() throws Exception {
+        return null;
+    }
+}
